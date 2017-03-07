9cad9ebdcae5fe3df0e27bac9dc20b2351c23cf8
tsleolima <leonardo.lima.silva@ccc.ufcg.edu.br>
2017-03-07 20:17:29 -0300

    Add files via upload

---
+++ Projeto/CentralDeProjetos/src/facade/Facade.java
@@ -0,0 +1,12 @@
+package facade;
+import easyaccept.EasyAccept;
+
+public class Facade {
+	
+	
+	public static void main(String[] args) {
+		args = new String[] {"facade.Facade", "acceptance_test/us1_test.txt"} ;
+		EasyAccept.main(args);
+		
+	}
+}
