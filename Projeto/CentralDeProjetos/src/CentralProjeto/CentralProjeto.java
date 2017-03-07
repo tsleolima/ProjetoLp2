87ebb5fef26a26692a16d116537d8c6711b4fb80
tsleolima <leonardo.lima.silva@ccc.ufcg.edu.br>
2017-03-07 20:14:53 -0300

    Add files via upload
    5° attemp

--- Projeto/CentralDeProjetos/src/CentralProjeto/CentralProjeto.java
+++ Projeto/CentralDeProjetos/src/CentralProjeto/CentralProjeto.java
@@ -6,55 +6,73 @@ import java.util.List;
 import Pessoa.Pessoa;
 
 public class CentralProjeto {
-		
-		List<Pessoa> pessoas = new ArrayList<>();
-		
-		public String cadastraPessoa(String cpf, String nome, String email) throws Exception{
-			Pessoa pessoa = new Pessoa(nome, email, cpf);
-			pessoas.add(pessoa);
-			return cpf;
+
+	List<Pessoa> pessoas = new ArrayList<>();
+
+	public String cadastraPessoa(String cpf, String nome, String email) throws Exception {
+		Pessoa pessoa = new Pessoa(nome, email, cpf);
+		pessoas.add(pessoa);
+		return cpf;
+	}
+
+	public void getInfoPessoa(String cpf, String atributo) throws Exception {
+		Pessoa pessoa = buscaPessoa(cpf);
+		if (pessoa == null) {
+			throw new Exception("Erro na consulta de pessoa: Pessoa nao encontrada");
 		}
-		
-		public void removePessoa(String cpf){
-			Pessoa pessoa = buscaPessoa(cpf);
-			pessoas.remove(pessoa);
-			
+
+		if (atributo.equalsIgnoreCase("Nome")) {
+			pessoa.getNome();
 		}
-		
-		public void listarPessoas(){
-			for (int i = 0; i < pessoas.size(); i++){
-				System.out.println(pessoas.get(i).getNome());
-				System.out.println(pessoas.get(i).getEmail());
-				System.out.println(pessoas.get(i).getCpf());
+
+		else if (atributo.equalsIgnoreCase("Email")) {
+			pessoa.getEmail();
+		} else {
+			throw new Exception("Erro na atualizacao de pessoa: Atributo inexistente");
+		}
+	}
+
+	private Pessoa buscaPessoa(String cpf) {
+		for (Pessoa i : pessoas) {
+			if (i.getCpf().equalsIgnoreCase(cpf)) {
+				return i;
 			}
 		}
-		
-		public Pessoa buscaPessoa(String cpf){
-			for (Pessoa i: pessoas){
-				if (i.getCpf().equals(cpf)){
-					return i;
-				}
-			}
-			return null;
-		}
-		
-		public void editaPessoa(String cpf,String atributo, String valor) throws Exception{
+		return null;
+	}
+
+	public boolean removePessoa(String cpf) throws Exception {
 			Pessoa pessoa = buscaPessoa(cpf);
 			if (pessoa == null){
 				throw new Exception("Erro na consulta de pessoa: Pessoa nao encontrada");
 			}
-			if (atributo.equalsIgnoreCase("Nome")){
-				pessoa.setNome(valor);
-			}
-			
-			else if (atributo.equalsIgnoreCase("Email")){
-				pessoa.setEmail(valor);
-			}
-			else if (atributo.equalsIgnoreCase("CPF")){
-				throw new Exception("Erro na atualizacao de pessoa: CPF nao pode ser modificado");
-			}
-			else{
-				throw new Exception("Erro na atualizacao de pessoa: Atributo inexistente");
-			}
+			pessoas.remove(pessoa);
+			return true;
+	}
+
+	public void listarPessoas() {
+		for (int i = 0; i < pessoas.size(); i++) {
+			System.out.println(pessoas.get(i).getNome());
+			System.out.println(pessoas.get(i).getEmail());
+			System.out.println(pessoas.get(i).getCpf());
 		}
+	}
+
+	public void atualizaPessoa(String cpf, String atributo, String valor) throws Exception {
+		Pessoa pessoa = buscaPessoa(cpf);
+		if (pessoa == null) {
+			throw new Exception("Erro na consulta de pessoa: Pessoa nao encontrada");
+		}
+		if (atributo.equalsIgnoreCase("Nome")) {
+			pessoa.setNome(valor);
+		}
+
+		else if (atributo.equalsIgnoreCase("Email")) {
+			pessoa.setEmail(valor);
+		} else if (atributo.equalsIgnoreCase("CPF")) {
+			throw new Exception("Erro na atualizacao de pessoa: CPF nao pode ser modificado");
+		} else {
+			throw new Exception("Erro na atualizacao de pessoa: Atributo inexistente");
+		}
+	}
 }
